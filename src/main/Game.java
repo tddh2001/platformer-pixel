8100341bec19af416bdfc03ba6ad117b6c5770d5
tddh2001 <ngtiendat181101@gmail.com>
2023-05-07 10:24:39 +0700

    update stage 1

--- src/main/Game.java
+++ src/main/Game.java
@@ -78,53 +78,40 @@ public class Game implements Runnable {
 	}
 
 	@Override
-	public void run() {
+		public void run() {
 		double timePerFrame = 1000000000.0 / FPS_SET;
 		double timePerUpdate = 1000000000.0 / UPS_SET;
-
 		long previousTime = System.nanoTime();
-
 		int frames = 0;
 		int updates = 0;
 		long lastCheck = System.currentTimeMillis();
-
 		double deltaU = 0;
 		double deltaF = 0;
-
 		while (true) {
-
 			long currentTime = System.nanoTime();
-
 			deltaU += (currentTime - previousTime) / timePerUpdate;
 			deltaF += (currentTime - previousTime) / timePerFrame;
 			previousTime = currentTime;
 
 			if (deltaU >= 1) {
-
 				update();
 				updates++;
 				deltaU--;
-
 			}
 
 			if (deltaF >= 1) {
-
 				gamePanel.repaint();
 				frames++;
 				deltaF--;
-
 			}
 
 			if (SHOW_FPS_UPS)
 				if (System.currentTimeMillis() - lastCheck >= 1000) {
-
 					lastCheck = System.currentTimeMillis();
 					System.out.println("FPS: " + frames + " | UPS: " + updates);
 					frames = 0;
 					updates = 0;
-
 				}
-
 		}
 	}
 
